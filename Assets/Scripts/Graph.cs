67165407958e173ded5337c1dac8a4869b0b5c2b
Bailee Vaughan <47824584+BaileeVaughan@users.noreply.github.com>
2019-09-18 12:07:17 +1000

    1

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -6,6 +6,7 @@ public class Graph : MonoBehaviour
 {
     public Transform pointPrefab;
     public int resolution = 10;
+    public GraphFunctionName function;
     Transform[] points;
     public int speed = 1;
 
@@ -16,11 +17,17 @@ public class Graph : MonoBehaviour
         Vector3 position;
         position.y = 0f;
         position.z = 0f;
-        points = new Transform[resolution];
-        for(int i = 0; i < resolution; i++)
+        points = new Transform[resolution * resolution];
+        for (int i = 0, x = 0, z = 0; i < resolution; i++, x++)
         {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
             Transform point = Instantiate(pointPrefab);
             position.x = (i + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
@@ -30,13 +37,32 @@ public class Graph : MonoBehaviour
 
     private void Update()
     {
+        float t = Time.time * speed;
+        GraphFunction f = functions[(int)function];
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time * speed));
+            position.y = f(position.x, position.z, t);
             point.localPosition = position;
         }
     }
 
+    static float SineFunction(float x, float z, float t)
+    {
+        return Mathf.Sin(Mathf.PI * (x + t));
+    }
+
+    static float MultiSineFunction(float x, float z, float t)
+    {
+        float y = Mathf.Sin(Mathf.PI * (x + t));
+        y += Mathf.Sin(2f * Mathf.PI * (x + 2f * t)) / 2f;
+        y *= 2f / 3f;
+        return y;
+    }
+
+    static GraphFunction[] functions =
+    {
+        SineFunction, MultiSineFunction
+    };
 }
